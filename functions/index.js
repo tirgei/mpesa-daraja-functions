c76b24e6f4c89ac7c2cf55d4476271610a7f4aff
Tirgei <tirgeic@gmail.com>
2018-05-24 23:36:28 +0300

    removed junk

--- functions/index.js
+++ functions/index.js
@@ -20,41 +20,13 @@ exports.welcomeUser = functions.database.ref('/users/{key}').onCreate((snapshot,
 		data: {
 			title: "\"Welcome " + name +"\"",
 			message: "\"We hope you'll enjoy the app\"",
-			image: "\"https://i1.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png\"",
+			image: "",
 			timestamp: new Date().getTime().toString()
 		}
 	};
 
 	return admin.messaging().sendToDevice(token, payload);
 	
-});
-
-exports.confirmPayment = functions.https.onRequest((request, response) => {
-	if(request.method === 'PUT'){
-    	response.status(400).send("ERROR: BAD REQUEST");
-  	}
-
-  	const userID = request.query.id;
-  	const userToken = request.query.token;
-
-	//console.log("User ID " + userID + " and token " + userToken);
-
-  	const payload = {
-  		notification: {
-  			title: "Transaction",
-  			body: "Processing your transaction..."
-  		}
-  	}
-
-	admin.messaging().sendToDevice(userToken, payload)
-	.then(function (res) {
-		response.status(200).send("Notification sent");
-	})
-	.catch(function (error){
-		console.log("Error sending notification:" + error);
-	});
-  	
-
 });
 
 exports.mpesaCallback = functions.https.onRequest((request, response) => {
