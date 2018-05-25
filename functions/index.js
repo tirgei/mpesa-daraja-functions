bf18e926ada06c5ab019abb36cd1284901a22ae3
Tirgei <tirgeic@gmail.com>
2018-05-25 17:46:08 +0300

    added function to update balance and register transaction in database

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,10 @@ admin.initializeApp(functions.config().firebase);
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
+
+/**
+ * This function sends a welcome notification to the user on sign up
+ */
 exports.welcomeUser = functions.database.ref('/users/{key}').onCreate((snapshot, context) => {
 	const user = snapshot.val();
 
@@ -14,9 +18,10 @@ exports.welcomeUser = functions.database.ref('/users/{key}').onCreate((snapshot,
 
 	const payload = {
 		data: {
+			type: "welcome",
 			title: "\"Welcome " + name +"\"",
 			message: "\"We hope you'll enjoy the app\"",
-			image: "",
+			image: "\"\"",
 			timestamp: new Date().getTime().toString()
 		}
 	};
@@ -25,9 +30,14 @@ exports.welcomeUser = functions.database.ref('/users/{key}').onCreate((snapshot,
 	
 });
 
+
+/**
+ * This function is the callback endpoint for Safaricom STK push
+ * Parameters received is user device token to send notification to
+ */
 exports.mpesaCallback = functions.https.onRequest((request, response) => {
 	if(request.method === 'PUT'){
-		response.status(400).send("ERROR: BAD REQUEST");
+		response.status(403).send("FORBIDDEN");
 	}
 
 	const userToken = request.query.token;
@@ -44,6 +54,63 @@ exports.mpesaCallback = functions.https.onRequest((request, response) => {
 	})
 	.catch(function (error){
 		console.log("Error sending notification:" + error);
+	});
+
+});
+
+
+/**
+ * This function updates the users account balance and also registers the transaction event in the database
+ */
+exports.updateWallet = functions.https.onRequest((request, response) => {
+	if(request.method === 'PUT'){
+		response.status(403).send("FORBIDDEN");
+	}
+
+	const checkoutId = request.query.checkoutId;
+	const userId = request.query.uid;
+	const token = request.query.userToken;
+	const amount = parseInt(request.query.amount);
+	const time = request.query.time;
+	const desc = request.query.description;
+	var balance;
+
+	var transactionId = admin.database().ref('transaction/' + userId).push().key;
+
+	const updateTransactionPromise = admin.database().ref('transactions/' + userId + "/" + transactionId).set({
+		id: transactionId,
+		checkoutId: checkoutId,
+		amount: amount,
+		description: desc,
+		time: time
+	});
+
+	const balancePromise = admin.database().ref('users/' + userId + '/balance').once('value');
+	const updateBalancePromise = balancePromise.then(snapshot => {
+		balance = parseInt(snapshot.val());
+		
+		if(balance){
+			balance = balance + amount;
+		} else {
+			balance = amount;
+		}
+
+		const setBalance = admin.database().ref('users/' + userId + '/balance').set(balance);
+
+		const payload = {
+			notification: {
+				title: "Transaction successful",
+				body: "Your wallet has been updated with amount " + amount + ". New balance is " + balance
+			}
+		}
+	
+		const sendNotification = admin.messaging().sendToDevice(token, payload);
+
+		return Promise.all([setBalance, sendNotification]);
+	});
+
+	return Promise.all([updateTransactionPromise, updateBalancePromise]).then(function(res){
+		response.status(200).send("Wallet update complete");
 	});
 
 });
